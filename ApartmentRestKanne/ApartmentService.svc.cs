8611bf14798b42f8ca16645e64f62db7ea6c947a
Anne Sofie Molsted Wanscher <anne55x9@edu.easj.dk>
2017-10-03 10:55:39 +0200

    Oprettet WCF projekt til appartmentREST og renamet Service1 efterfulgt af renaming IService1.

--- ApartmentRestKanne/ApartmentService.svc.cs
+++ ApartmentRestKanne/ApartmentService.svc.cs
@@ -8,9 +8,9 @@ using System.Text;
 
 namespace ApartmentRestKanne
 {
-    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
-    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
-    public class Service1 : IService1
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "ApartmentService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select ApartmentService.svc or ApartmentService.svc.cs at the Solution Explorer and start debugging.
+    public class ApartmentService : IApartmentService
     {
         public string GetData(int value)
         {
--- ApartmentRestKanne/IApartmentService.cs
+++ ApartmentRestKanne/IApartmentService.cs
@@ -8,9 +8,9 @@ using System.Text;
 
 namespace ApartmentRestKanne
 {
-    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IApartmentService" in both code and config file together.
     [ServiceContract]
-    public interface IService1
+    public interface IApartmentService
     {
 
         [OperationContract]
